c40df7cacc91c43f357100f14fe0733b0cdfbd68
Julian <nunezjulian505@gmail.com>
2023-10-03 22:10:56 -0600

    Proyecto 2 terminado

--- src/Metodos.java
+++ src/Metodos.java
@@ -3,36 +3,102 @@ import java.util.Scanner;
 
 public class Metodos {
     File archivo = new File("C:\\Users\\win\\OneDrive\\Escritorio\\archivos\\archivositemasoperativos\\productos.txt");
+    File archivo2 = new File("C:\\Users\\win\\OneDrive\\Escritorio\\archivos\\archivositemasoperativos\\productos2.txt");
     Scanner lector = new Scanner(System.in);
 
     public void alta() throws IOException {
-        FileWriter escribir = new FileWriter(archivo, true);
-        //(ruta del archivo, que se quiere anadir al archivo y no sobreescribir);
+
         System.out.println("Ingresa la clave del producto");
-        String clave = lector.nextLine();
-        escribir.write("Clave :" + clave + "\n");
+        int clave = lector.nextInt();
+        // Consumir el carácter de nueva línea restante
+        lector.nextLine();
 
         System.out.println("Ingresa el nombre del producto");
         String nombre = lector.nextLine();
-        escribir.write("Nombre :" + nombre + "\n");
 
         System.out.println("Ingresa la marca del producto");
         String marca = lector.nextLine();
-        escribir.write("Marca :" + marca + "\n");
 
         System.out.println("Ingresa el precio del producto");
         double precio = Double.parseDouble(lector.nextLine());
         //se cambia el tipo de dato
-        escribir.write("Precio :" + precio + "\n");
 
         System.out.println("Ingresa la cantidad del producto");
         int cantidad = Integer.parseInt(lector.nextLine());
         //tambien aqui se cambia el tipo de dato
-        escribir.write("Cantidad :" + cantidad + "\n");
-        escribir.close();
+
+        if(clave>10){
+            archivo=archivo2;
+        }else{
+            archivo= new File("C:\\Users\\win\\OneDrive\\Escritorio\\archivos\\archivositemasoperativos\\productos.txt");
+        }
+
+        File archivoTemporal = new File(archivo.getAbsolutePath() + ".tmp");
+        // Creación del nuevo archivo temporal
+        BufferedReader lectora = new BufferedReader(new FileReader(archivo));
+        // Buffer para leer
+        FileWriter escritor = new FileWriter(archivoTemporal);
+        // Buffer para escribir
+        int contador =0;
+
+        String linea;
+        boolean productoEncontrado = false;
+
+        while ((linea = lectora.readLine()) != null) {
+            if (linea.contains("*") && contador==0) {
+                //aqui vamos a escribir el nueva producto
+                for(int i=0; i<4;i++){
+                    lectora.readLine();
+                    // nos pasamos las lineas donde esta el producto
+                }
+                productoEncontrado = true;// Producto encontrado, no escribirlo en el archivo temporal
+                escritor.write("Clave :" + clave + "\n");
+                escritor.write("Nombre :" + nombre + "\n");
+                escritor.write("Marca :" + marca + "\n");
+                escritor.write("Precio :" + precio + "\n");
+                escritor.write("Cantidad :" + cantidad + "\n");
+                contador++;
+            } else {
+                escritor.write(linea + "\n");
+            }
+
+        }
+        escritor.close();
+        lectora.close();
+
+
+        if (!productoEncontrado) {
+
+            archivoTemporal.delete();
+            // no tiene sentido por que al final es el mismo texto
+            escritor = new FileWriter(archivo,true);
+            escritor.write("Clave :" + clave + "\n");
+            escritor.write("Nombre :" + nombre + "\n");
+            escritor.write("Marca :" + marca + "\n");
+            escritor.write("Precio :" + precio + "\n");
+            escritor.write("Cantidad :" + cantidad + "\n");
+            escritor.close();
+
+        } else {
+            archivo.delete();
+            archivoTemporal.renameTo(archivo);//se renombra el archivo a la ruta del archivo
+        }
+
     }
 
+
+
+
     public void borrar() throws IOException {
+
+        System.out.println("Ingresa la clave del producto a borrar");
+        int clave =  lector.nextInt();
+
+        if(clave >10){
+            archivo=archivo2;
+        }else{
+            archivo= new File("C:\\Users\\win\\OneDrive\\Escritorio\\archivos\\archivositemasoperativos\\productos.txt");
+        }
 
         File archivoTemporal = new File("C:\\Users\\win\\OneDrive\\Escritorio\\archivos\\archivositemasoperativos\\productostem.txt");
         // Creación del nuevo archivo temporal
@@ -41,15 +107,14 @@ public class Metodos {
         BufferedWriter escritor = new BufferedWriter(new FileWriter(archivoTemporal));
         // Buffer para escribir
 
-        System.out.println("Ingresa la clave del producto a borrar");
-        String Claveborrar = "Clave :" + lector.nextLine();
 
+        String Claveborrar = "Clave :" + clave;
         String linea;
         boolean productoEncontrado = false;
 
         while ((linea = lectora.readLine()) != null) {
             if (linea.contains(Claveborrar)) {
-                // aqui vamos a escribir el asterisco para identificar que lo borramos
+                // aqui vamos a describe el asterisco para identificar que lo borramos
                 escritor.write("* "+linea+"\n");
                 // Producto encontrado, no escribirlo en el archivo temporal
                 productoEncontrado = true;
@@ -77,14 +142,24 @@ public class Metodos {
 
 
 public void modificar() throws IOException {
+
+    System.out.println("Dame la clave del producto a modificar");
+    int cla = lector.nextInt();
+    String claveAModificar = "Clave :"+cla;
+
+    if(cla>10){
+        archivo=archivo2;
+    }else{
+        archivo= new File("C:\\Users\\win\\OneDrive\\Escritorio\\archivos\\archivositemasoperativos\\productos.txt");
+    }
+
     File archivoTemporal = new File("C:\\Users\\win\\OneDrive\\Escritorio\\archivos\\archivositemasoperativos\\productostem.txt");
     // Creación del nuevo archivo temporal
     BufferedReader lectora = new BufferedReader(new FileReader(archivo));
     // Buffer para leer
     BufferedWriter escritor = new BufferedWriter(new FileWriter(archivoTemporal));
     // Buffer para escribir
-    System.out.println("Dame la clave del producto a modificar");
-    String claveAModificar = "Clave :"+lector.nextLine();
+
     String linea;
     boolean encontrado =false;
     while((linea = lectora.readLine()) !=null){
@@ -93,9 +168,10 @@ public void modificar() throws IOException {
                 lectora.readLine();
             }
             encontrado=true;
-            System.out.println("Ingresa la nueva clave del producto");
-            String clave = lector.nextLine();
-            escritor.write("Clave :" + clave + "\n");
+
+ //dejamos la misma clave del producto
+            escritor.write("Clave :" + cla + "\n");
+            lector.nextLine();
 
             System.out.println("Ingresa el nuevo nombre del producto");
             String nombre = lector.nextLine();
@@ -125,26 +201,44 @@ public void modificar() throws IOException {
         System.out.println("No se encontro el producto a modificar");
         archivoTemporal.delete();
     }else{
-       /* System.out.println("Archivo modificado");
+        System.out.println("Archivo modificado");
         archivo.delete();
-        archivoTemporal.renameTo(archivo);*/
+        archivoTemporal.renameTo(archivo);
 
+    }
+}
+    public void mostrar() throws IOException {
+
+        BufferedReader lectora = new BufferedReader(new FileReader(archivo));
+        String cadena;
+        archivo = new File("C:\\Users\\win\\OneDrive\\Escritorio\\archivos\\archivositemasoperativos\\productos.txt");
+
+        while ((cadena = lectora.readLine()) != null) {
+            if (cadena.contains("*")) {
+                for (int i = 0; i < 4; i++) {
+                    lectora.readLine();
+                }
+            } else {
+                System.out.println(cadena);
+            }
+        }
+        // Cerrar el primer archivo
+        lectora.close();
+
+        // Crear un nuevo bufer para el archivo2
+        BufferedReader lectora2 = new BufferedReader(new FileReader(archivo2));
+        archivo2 = new File("C:\\Users\\win\\OneDrive\\Escritorio\\archivos\\archivositemasoperativos\\productos2.txt");
+
+        while ((cadena = lectora2.readLine()) != null) {
+            if (cadena.contains("*")) {
+                for (int i = 0; i < 4; i++) {
+                    lectora2.readLine();
+                }
+            } else {
+                System.out.println(cadena);
+            }
+        }
+        lectora2.close();
     }
 
 }
-public void mostrar()throws IOException{
-    BufferedReader lectora = new BufferedReader(new FileReader(archivo));
-String cadena;
-if(archivo.exists()){
-    while ((cadena= lectora.readLine()) != null){
-        System.out.println(cadena);
-}
-    System.out.println("\n");
-    lectora.close();
-}else{
-    System.out.println("El archivo no existe");
-}
-}
-    }
-
-
